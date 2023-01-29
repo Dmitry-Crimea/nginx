393a6d6d4c94f81a03a798e5c0cdf9bd1fe81f47
dmitry.brykin <dmitry.brykin@ax-team.com>
2023-01-29 23:14:13 +0300

    lesson7

--- lesson_07/cert.sh
+++ lesson_07/cert.sh
@@ -1,5 +1,12 @@
-sudo apt update && sudo apt install nginx -y
-sudo apt install apache2-utils -y
-htpasswd -n user
- >- ввести пароль: test
-vim /etc/nginx/.htpasswd
+sudo apt update && sudo apt install nginx && apt install letsencrypt -y
+mkdir -p /opt/www/acme
+letsencrypt certonly --webroot -w /opt/www/acme -d ssl.3b54d9c4446225f6721ce16370cec007.kis.im
+
+mkdir -p /etc/nginx/ssl
+
+openssl genrsa -out ca.key 2048
+openssl req -new -x509 -days 3650 -key ca.key -out ca.crt
+
+openssl genrsa -out client.key 2048
+openssl req -new -key client.key -out client.csr
+openssl x509 -req -days 3650 -in client.csr -CA ca.pem -CAkey ca.key -CAcreateserial -out client.crt -days 5000
